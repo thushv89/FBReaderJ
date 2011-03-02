a2ce74f116e513bc42a062e7d449d19558fe1d54
Abhi Mahule <Abhi@Abhi-Mahules-MacBook-Pro.local>
2011-03-02 12:13:35 -0600

    File that will contain the developer key generated by Bookshare

---
+++ src/org/geometerplus/android/fbreader/network/bookshare/BookshareDeveloperKey.java
@@ -0,0 +1,12 @@
+package org.geometerplus.android.fbreader.network.bookshare;
+
+/**
+ * Enter your developer key here.
+ * It will be read by other Bookshare classes
+ *
+ */
+public class BookshareDeveloperKey {
+	
+	// Enter your developer key here
+	public static final String DEVELOPER_KEY = "";
+}
