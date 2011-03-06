1757ef57da63332c28b27f1424e138d74fb145fa
Abhi Mahule <Abhi@Abhi-Mahules-MacBook-Pro.local>
2011-03-06 13:28:16 -0600

    A POJO for storing the member details retrieved as a result of OM list response

---
+++ src/org/geometerplus/android/fbreader/network/bookshare/Bookshare_OM_Member_Bean.java
@@ -0,0 +1,32 @@
+package org.geometerplus.android.fbreader.network.bookshare;
+
+public class Bookshare_OM_Member_Bean {
+	private String memberId;
+	private String firstName;
+	private String LastName;
+
+	/**
+	 * Getters and Setters
+	 */
+	public void setMemberId(String memberId){
+		this.memberId = memberId;
+	}
+	public String getMemberId(){
+		return memberId;
+	}
+	
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	public String getFirstName(){
+		return this.firstName;
+	}
+	
+	public void setLastName(String lastName){
+		this.LastName = lastName;
+	}
+	public String getlastName(){
+		return LastName;
+	}
+	
+}
