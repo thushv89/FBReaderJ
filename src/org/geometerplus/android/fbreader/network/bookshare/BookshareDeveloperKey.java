7bf964912cdddacb103f018dc05b4f7635f7189d
Abhi Mahule <abhijeetmahule@gmail.com>
2011-09-07 19:04:04 -0700

    Trying to remove the file

--- src/org/geometerplus/android/fbreader/network/bookshare/BookshareDeveloperKey.java
+++ src/org/geometerplus/android/fbreader/network/bookshare/BookshareDeveloperKey.java
@@ -7,6 +7,7 @@ package org.geometerplus.android.fbreader.network.bookshare;
  */
 public class BookshareDeveloperKey {
 	
+	
 	// Enter your developer key here
 	public static final String DEVELOPER_KEY = " ";
 }
