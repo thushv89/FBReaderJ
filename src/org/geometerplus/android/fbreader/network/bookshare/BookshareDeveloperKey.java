deaed526d7ea5b5f3062df20a64ccf6600fe1709
Abhi Mahule <abhijeetmahule@gmail.com>
2011-09-08 10:51:44 -0700

    The file got deleted from repo, so recommitting it.

---
+++ src/org/geometerplus/android/fbreader/network/bookshare/BookshareDeveloperKey.java
@@ -0,0 +1,12 @@
+package org.geometerplus.android.fbreader.network.bookshare;
+
+/**
+ * Enter your developer key here.
+ * It will be read by other Bookshare classes
+ * You can obtain a developer key from http://developer.bookshare.org
+ */
+public class BookshareDeveloperKey {
+	
+	// Enter your developer key here
+	public static final String DEVELOPER_KEY = "";
+}
