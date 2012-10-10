26b5ccead3f2f360ac4bdafae35696da44459f4c
Gerardo Capiel <gcapiel@alum.mit.edu>
2012-10-10 14:47:13 -0700

    Revert "additions to gitignore"
    This reverts commit 714772a29a120fc023307fbaded45c9781a21bda.

---
+++ src/org/geometerplus/android/fbreader/network/bookshare/BookshareDeveloperKey.java
@@ -0,0 +1,13 @@
+package org.geometerplus.android.fbreader.network.bookshare;
+
+/**
+ * Enter your developer key here.
+ * It will be read by other Bookshare classes
+ * You can obtain a developer key from http://developer.bookshare.org
+ */
+public class BookshareDeveloperKey {
+	
+	// Enter your developer key here
+	public static final String DEVELOPER_KEY = "";
+    public static final String BUGSENSE_KEY = "";
+}
